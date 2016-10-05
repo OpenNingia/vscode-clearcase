7bcc96bd6fe1b49424a79eaf630e84b99973c62c
OpenNingia <oppifjellet@gmail.com>
2016-10-05 11:54:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# VS Code ClearCase
+
+This repository contains the extension for [IBM Rational ClearCase SCM](http://www-03.ibm.com/software/products/en/clearcase) for the [VS Code](https://code.visualstudio.com) editor.
+
+## Features
+
+Clearcase commands exposed by this extensions:
+
+    * Launch ClearCase Explorer
+    * Checkout
+    * Checkin
+    * Undo Checkout
+    * Version Tree
+    * Compare with previous version
+
+All commands are relative to the current file.
+
+## Requirements
+
+A valid ClearCase installation within the system's PATH.
+
+## Release Notes
+
+Nothing here yet...
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,106 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {exec, execSync} from 'child_process'
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vscode-clearcase" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.ccExplorer', () => {
+        // The code you place here will be executed every time your command is executed               
+        var current_file = vscode.window.activeTextEditor.document.fileName;
+        execOnSCMFile(current_file, runClearCaseExplorer);
+    });
+
+    context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('extension.ccCheckout', () => {
+        // The code you place here will be executed every time your command is executed               
+        var current_file = vscode.window.activeTextEditor.document.fileName;
+        execOnSCMFile(current_file, checkoutFile);
+    });
+
+    context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('extension.ccCheckin', () => {
+        // The code you place here will be executed every time your command is executed               
+        var current_file = vscode.window.activeTextEditor.document.fileName;
+        execOnSCMFile(current_file, checkinFile);
+    });
+
+    context.subscriptions.push(disposable);    
+
+    disposable = vscode.commands.registerCommand('extension.ccVersionTree', () => {
+        // The code you place here will be executed every time your command is executed               
+        var current_file = vscode.window.activeTextEditor.document.fileName;
+        execOnSCMFile(current_file, versionTree);
+    });
+
+    context.subscriptions.push(disposable);    
+
+    disposable = vscode.commands.registerCommand('extension.ccComparePrevious', () => {
+        // The code you place here will be executed every time your command is executed               
+        var current_file = vscode.window.activeTextEditor.document.fileName;
+        execOnSCMFile(current_file, diffWithPrevious);
+    });
+
+    context.subscriptions.push(disposable);    
+
+    disposable = vscode.commands.registerCommand('extension.ccUndoCheckout', () => {
+        // The code you place here will be executed every time your command is executed               
+        var current_file = vscode.window.activeTextEditor.document.fileName;
+        execOnSCMFile(current_file, undoCheckoutFile);
+    });
+
+    context.subscriptions.push(disposable);                    
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+function execOnSCMFile(path: string, func: (string) => void)
+{
+    exec('cleartool ls ' + path, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    func(path);
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    });
+}
+
+function runClearCaseExplorer(path: string) {
+    exec('clearexplorer ' + path);  
+}
+
+function checkoutFile(path: string) {
+    exec('cleardlg /checkout ' + path);  
+}
+
+function undoCheckoutFile(path: string) {
+    exec('cleartool unco ' + path);  
+}
+
+function checkinFile(path: string) {
+    exec('cleardlg /checkin ' + path);  
+}
+
+function versionTree(path: string) {
+    exec('cleartool lsvtree -graphical ' + path);  
+}
+
+function diffWithPrevious(path: string) {
+    exec('cleartool diff -graph -pred ' + path);  
+}
