fd777ec1bd7ec223101b79186bb57f32c65acbfc
Frank <dev@seito.de>
2024-09-06 01:30:04 +0200

    getting extension context

--- src/test/bin/cleartool.sh
+++ src/test/bin/cleartool.sh
@@ -1,35 +1,83 @@
 #!/bin/bash
 
+doCheckin=0
+doCheckout=0
+doUncheckout=0
+doNoComment=0
+doComment=0
+doTree=0
+doTreeGrafical=0
+doConfigspec=0
+doDescribe=0
+
+theComment=""
+theFormat=""
+
+usage() {
+  echo "cleartool.sh - simulate cleartool for test purpose"
+  echo ""
+  echo "cleartool.sh [--comment msg|-nc]"
+  echo "options:"
+  echo "-h, --help      show this message"
+  echo "--comment       show this message"
+}
+
+checkin() {
+  echo "checkin version"
+}
+
 # $@ is all command line parameters passed to the script.
 # -o is for short options like -v
 # -l is for long options with double dash like --version
 # the comma separates different long options
 # -a is for long options with single dash like -version
-options=$(getopt -l "help,comment:,checkout,checkin,lsvtree,ci,co,nc" -o "h" -a -- "$@")
 
-while true
-do
-case "$1" in
--h|--help) 
-    showHelp
+# options=$(getopt -l "help,comment:,checkout,checkin,lsvtree" -o "h" -a "nc,fmt:,grafical" "$@")
+# eval set -- "$options"
+
+while true; do
+  case "$1" in
+  -h | --help)
+    usage
     exit 0
     ;;
--v|--version) 
+  ci | checkin)
+    checkin
+    ;;
+  co | checkout)
+    doCheckout=1
+    ;;
+  unco | uncheckout)
+    doCheckout=1
+    ;;
+  lsvtree)
+    doTree=1
+    ;;
+  unco | uncheckout)
+    doCheckout=1
+    ;;
+  -nc)
+    doNoComment=1
+    ;;
+  -grahpical)
+    doTreeGrafical=1
+    ;;
+  --comment)
     shift
-    export version="$1"
+    theComment="$1"
+    doComment=1
     ;;
--V|--verbose)
-    export verbose=1
-    set -xv  # Set xtrace and verbose mode.
-    ;;
--r|--rebuild)
-    export rebuild=1
-    ;;
---)
+  -fmt)
     shift
-    break;;
-esac
-shift
+    theFormat="$1"
+    ;;
+  --)
+    shift
+    break
+    ;;
+  esac
+  shift
+  if [ $#==0 ]; then
+    break
+  fi
 done
-
-
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,35 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import { CCConfigHandler } from '../../ccConfigHandler';
+import { CCScmProvider } from '../../ccScmProvider';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
+	let extensionContext: vscode.ExtensionContext;
+	let outputChannel: vscode.OutputChannel;
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	suiteSetup( async () => {
+		await vscode.extensions.getExtension("vscode-clearcase")?.activate();
+		/* eslint-disable */
+		extensionContext = (global as any).testExtensionContext;
+		/* eslint-enable */
+	});
+
+	test('Cleartool change executable', () => {
+		const configHandler = new CCConfigHandler();
+		configHandler.configuration.executable.value = "bin/cleartool.sh";
+		assert.strictEqual(configHandler.configuration.executable.value, "bin/cleartool.sh");
+	});
+
+	test('Cleartool checkin file', () => {
+		const configHandler = new CCConfigHandler();
+		const provider = new CCScmProvider(extensionContext, outputChannel, configHandler);
+		configHandler.configuration.executable.value = `bin/cleartool.sh`;
+
+		const file = vscode.Uri.parse("testfiles/simple01.txt");
+		provider.clearCase?.checkinFile([file]);
+		assert.strictEqual("blubb", "checkin version");
 	});
 });
